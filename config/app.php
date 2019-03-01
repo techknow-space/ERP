0d713dc240109520177d6c5eb24da5879e63d435
Maharshi Raval <mjraval@users.noreply.github.com>
2019-03-01 12:43:33 -0500

    Created an Empty blade service provider and registered it.

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\BladeServiceProvider::class,
         Laravel\Scout\ScoutServiceProvider::class,
         ScoutEngines\Elasticsearch\ElasticsearchProvider::class,
 
