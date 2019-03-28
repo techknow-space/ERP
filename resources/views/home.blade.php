3a27de0493fce5c2d2ff8677dffcb7b29fd8e109
Kirtaner <kirtaner@homelab.420chan.org>
2019-03-28 20:50:26 +0000

    home stub

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">ERP</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,8 +14,19 @@
                         </div>
                     @endif
 
-                    You are logged in
+                    Select task
                 </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Inventory</div>
+
+                <div class="card-body">
+                    <div class="list-group">
+                        <a href="/" class="list-group-item list-group-item-action">TASK</a>
+                    </div>
+               </div>
             </div>
         </div>
     </div>
