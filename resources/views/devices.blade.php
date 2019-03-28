81fdb24819ae019e916180ffe2789bac24480fd7
Maharshi Raval <mjraval@users.noreply.github.com>
2019-03-28 11:37:57 -0400

    Fixed issue for Parts with Parent

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -31,13 +31,32 @@
                             <tbody>
                                 @foreach($device->parts as $part)
                                     <tr>
-                                        <td><a href="/itemlookup/sku/{{ $part->sku }}">{{ $part->part_name }}</a></td>
-                                        <td >${{ $part->price->selling_price_b2c }}</td>
-                                        @foreach ($part->stock as $qty)
-                                            <td>{{ $qty->stock_qty }}</td>
-                                        @endforeach
-                                        <td >${{ $part->price->last_cost }}</td>
-                                        <td>{{ $part->sku }}</td>
+                                    @if($part->is_child)
+                                            <td>
+                                                <a href="/itemlookup/sku/{{ $part->ParentPart->sku }}">{{ $part->part_name }}</a><br>
+                                                Compatible Part: <a href="/itemlookup/sku/{{ $part->ParentPart->sku }}">{{ $part->ParentPart->devices->brand->name }} {{ $part->ParentPart->devices->model_name }} <b>{{ $part->ParentPart->part_name }}</b></a>
+                                            </td>
+                                            <td >
+                                                @if(null !== $part->price)
+                                                    ${{ $part->price->selling_price_b2c }}
+                                                @else
+                                                    ${{ $part->ParentPart->price->selling_price_b2c }}
+                                                @endif
+                                            </td>
+                                            @foreach ($part->ParentPart->stock as $qty)
+                                                 <td>{{ $qty->stock_qty }}</td>
+                                            @endforeach
+                                            <td >${{ $part->ParentPart->price->last_cost }}</td>
+                                            <td>{{ $part->ParentPart->sku }}</td>
+                                    @else
+                                            <td><a href="/itemlookup/sku/{{ $part->sku }}">{{ $part->part_name }}</a></td>
+                                            <td >${{ $part->price->selling_price_b2c }}</td>
+                                            @foreach ($part->stock as $qty)
+                                                <td>{{ $qty->stock_qty }}</td>
+                                            @endforeach
+                                            <td >${{ $part->price->last_cost }}</td>
+                                            <td>{{ $part->sku }}</td>
+                                    @endif
                                     </tr>
                                 @endforeach
                             </tbody>
