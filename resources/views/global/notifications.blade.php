369f57561144521cee675cfb5cb04816029b099a
Maharshi Raval <mjraval@users.noreply.github.com>
2019-04-01 13:07:48 -0400

    Added {{{ for the MArkup to show Correctly.

--- resources/views/global/notifications.blade.php
+++ resources/views/global/notifications.blade.php
@@ -3,7 +3,7 @@
         @if(session()->has('success'))
             @foreach(session('success') as $message)
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{$message}}
+                    {{{$message}}}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -13,7 +13,7 @@
         @if(session()->has('error'))
             @foreach(session('error') as $message)
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {{$message}}
+                    {{{$message}}}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
