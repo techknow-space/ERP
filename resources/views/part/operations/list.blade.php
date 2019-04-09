4fe868c0e771f472aab739eeac2d3c5599bf1dc1
Maharshi Raval <mjraval@users.noreply.github.com>
2019-04-09 17:48:02 -0400

    Added list of operations.

--- resources/views/part/operations/list.blade.php
+++ resources/views/part/operations/list.blade.php
@@ -8,7 +8,17 @@
                     <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#use-part-in-repair-dialog">Use Part in Repair</a>
                     <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#return-part-from-repair-dialog">Return Part from Repair</a>
                     <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="">Store in Defective Bin</a>
-                    <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#update-part-price-dialog">Update Part Price</a>
+                    <div class="input-group list-group-item list-group-item-action">
+                        <div class="input-group-prepend">
+                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Part Edit Operations</button>
+                            <div class="dropdown-menu ">
+                                <a href="#" class="dropdown-item" data-toggle="modal" data-target="#update-part-price-dialog">Update Part Price</a>
+                                <a href="#" class="dropdown-item" data-toggle="modal" data-target="#update-part-name-dialog">Update Part Name</a>
+                                <a href="#" class="dropdown-item" data-toggle="modal" data-target="#update-part-stock-dialog">Update Part Stock</a>
+                            </div>
+                        </div>
+                    </div>
+
                     <!-- <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#return-part-from-repair-dialog">Return Part to Defective</a> -->
 
                     <!-- <a href="#" class="list-group-item list-group-item-action">Begin Stock Count</a> -->
@@ -22,3 +32,5 @@
 @include('part.operations.modals.use')
 @include('part.operations.modals.return')
 @include('part.operations.modals.updatePartPrice')
+@include('part.operations.modals.updatePartName')
+@include('part.operations.modals.updatePartStock')
