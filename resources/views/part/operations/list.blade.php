61f490062536bf0416db6e7ca2ff8ce5b9daffb8
Maharshi Raval <mjraval@users.noreply.github.com>
2019-04-09 12:48:08 -0400

    Bit of House Keeping.

--- resources/views/part/operations/list.blade.php
+++ resources/views/part/operations/list.blade.php
@@ -6,7 +6,8 @@
             <div class="card-body">
                 <div class="list-group">
                     <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#use-part-in-repair-dialog">Use Part in Repair</a>
-                    <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#return-part-from-repair-dialog">Return Part to Inventory</a>
+                    <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#return-part-from-repair-dialog">Return Part from Repair</a>
+                    <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="">Store in Defective Bin</a>
                     <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#update-part-price-dialog">Update Part Price</a>
                     <!-- <a href="#" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#return-part-from-repair-dialog">Return Part to Defective</a> -->
 
